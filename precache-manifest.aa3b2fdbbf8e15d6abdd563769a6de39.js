13aa66d229b6d9e7350517d0dbeeef258d49806e
Yonatan Snir <yonatan941@gmail.com>
2019-11-19 10:07:22 +0200

    Updates

---
+++ precache-manifest.aa3b2fdbbf8e15d6abdd563769a6de39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58787a110721bc8e1ec93ebd9afd8c15",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "163142846b6110cb18b8",
+    "url": "/my-app/static/css/main.5da1929f.chunk.css"
+  },
+  {
+    "revision": "3c74012a76ae2c0ca166",
+    "url": "/my-app/static/js/2.6ab9199b.chunk.js"
+  },
+  {
+    "revision": "163142846b6110cb18b8",
+    "url": "/my-app/static/js/main.1ad07ce5.chunk.js"
+  },
+  {
+    "revision": "91f82d5fc0acb832fb3e",
+    "url": "/my-app/static/js/runtime-main.3673068e.js"
+  }
+]);
